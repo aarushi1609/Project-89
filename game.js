9d0cc191ed13dd1d48cf409e3b52ff8460736f10
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2021-11-23 09:42:09 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ player2_name = localStorage.getItem("player2_name");
 document.getElementById("player1_name").innerHTML = player1_name;
 document.getElementById("player2_name").innerHTML = player2_name;
 
+player1_score = 0;
+player2_score = 0;
+
 function send()
 {
     number1 = document.getElementById("number_1_input").value;
@@ -19,4 +22,43 @@ function send()
 
     document.getElementById("number_1_input").value = " ";
     document.getElementById("number_2_input").value = " ";
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            player1_score = player1_score+1;
+            document.getElementById("player1_score").innerHTML ="= "+ player1_score;
+        }
+        else
+        {
+            player2_score = player2_score+1;
+            document.getElementById("player2_score").innerHTML = "= "+player2_score;
+        }
+    }
+
+    if(answer_turn == "player1")
+    {
+        answer_turn = "player2";
+        question_turn = "player1";
+        document.getElementById("question_turn").innerHTML = "Question Turn = " + player1_name;
+        document.getElementById("answer_turn").innerHTML = "Answer Turn = " + player2_name;
+    }
+    else
+    {
+        answer_turn = "player1";
+        question_turn = "player2";
+        document.getElementById("question_turn").innerHTML = "Question Turn = " + player2_name;
+        document.getElementById("answer_turn").innerHTML = "Answer Turn = " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = " "
 }

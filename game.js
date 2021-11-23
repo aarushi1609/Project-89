d00791590d5e5f197a25175cd6d354f6e208fb7f
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2021-11-23 09:29:05 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -3,3 +3,20 @@ player2_name = localStorage.getItem("player2_name");
 
 document.getElementById("player1_name").innerHTML = player1_name;
 document.getElementById("player2_name").innerHTML = player2_name;
+
+function send()
+{
+    number1 = document.getElementById("number_1_input").value;
+    number2 = document.getElementById("number_2_input").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button id='check_button' onclick='check();'>Check</button>";
+
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+
+    document.getElementById("number_1_input").value = " ";
+    document.getElementById("number_2_input").value = " ";
+}
